7e3a51500c725426502884584ecfc49ec663e5d1
Mike D Pilsbury <mike.pilsbury@gmail.com>
2024-03-06 16:34:16 +0000

    Correct git ignored files follow reorg of lib dir

--- cgo.go
+++ cgo.go
@@ -1,9 +1,9 @@
 package opus
 
 // #cgo CFLAGS: -I${SRCDIR}/lib/opus
-// #cgo CFLAGS: -I${SRCDIR}/lib/opus/src
-// #cgo CFLAGS: -I${SRCDIR}/lib/opus/include
 // #cgo CFLAGS: -I${SRCDIR}/lib/opus/celt
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus/include
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus/src
 // #cgo CFLAGS: -I${SRCDIR}/lib/opus/silk
 // #cgo CFLAGS: -I${SRCDIR}/lib/opus/silk/float
 //
