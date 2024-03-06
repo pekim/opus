ab0f50f5e7a790ddd3a32ed37f70c713a0a41fe0
Mike D Pilsbury <mike.pilsbury@gmail.com>
2024-03-06 16:29:21 +0000

    Reorganise lib dir
    3 dirs in lib dir, one for each of 3 opus git repos

--- cgo.go
+++ cgo.go
@@ -1,11 +1,11 @@
 package opus
 
-// #cgo CFLAGS: -I${SRCDIR}/lib
-// #cgo CFLAGS: -I${SRCDIR}/lib/src
-// #cgo CFLAGS: -I${SRCDIR}/lib/include
-// #cgo CFLAGS: -I${SRCDIR}/lib/celt
-// #cgo CFLAGS: -I${SRCDIR}/lib/silk
-// #cgo CFLAGS: -I${SRCDIR}/lib/silk/float
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus/src
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus/include
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus/celt
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus/silk
+// #cgo CFLAGS: -I${SRCDIR}/lib/opus/silk/float
 //
 // #cgo CFLAGS: -I${SRCDIR}/lib/opusfile
 // #cgo CFLAGS: -I${SRCDIR}/lib/opusfile/src
