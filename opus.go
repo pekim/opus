5cb828e20ac27def4b05144151bf54f0d05f4215
Mike D Pilsbury <mike.pilsbury@gmail.com>
2024-03-07 06:46:24 +0000

    Remove unused code

--- opus.go
+++ opus.go
@@ -11,10 +11,6 @@ import (
 )
 
 func Temp() {
-	// fmt.Println(C.GoString(C.opus_get_version_string()))
-
-	// panic(errorFromOpusFileError(-133))
-
 	data, err := os.ReadFile("/home/mike/Music/The Chicks - Travelin' Soldier (Official Video) [AbfgxznPmZM].opus")
 	if err != nil {
 		panic(err)
@@ -33,37 +29,23 @@ func Temp() {
 	fmt.Println(decoder.TagsVendor())
 	fmt.Println(decoder.TagsUserComments())
 
-	var opusFileErr C.int
-	oggOpusFile := C.op_open_memory((*C.uchar)(&data[0]), C.size_t(len(data)), &opusFileErr)
-	if opusFileErr < 0 {
-		panic(errorFromOpusFileError(opusFileErr))
-	}
-
-	li := C.op_current_link(oggOpusFile)
-	channel_count := C.op_channel_count(oggOpusFile, li)
-	pcmTotal := C.op_pcm_total(oggOpusFile, li)
-	fmt.Println(pcmTotal, pcmTotal/48_000, pcmTotal/48_000/60, pcmTotal/48_000%60)
-	// bitrate := C.op_bitrate(oggOpusFile, li)
-	// fmt.Println("bitrate", bitrate)
-
-	fmt.Println()
-
-	var pcm = make([]int16, 20_000)
-	samplesReadPerChannel := C.op_read(oggOpusFile,
-		(*C.opus_int16)(&pcm[0]),
-		C.int(cap(pcm)), // / channel_count,
-		&li,
-	)
-	if samplesReadPerChannel < 0 {
-		panic(errorFromOpusFileError(samplesReadPerChannel))
-	}
-
-	fmt.Println(li, samplesReadPerChannel, samplesReadPerChannel*channel_count)
-	fmt.Println(pcm[:100])
-	i := samplesReadPerChannel * channel_count
-	fmt.Println(pcm[i-20 : i+20])
-
 	decoder.Close()
+
+	// var pcm = make([]int16, 20_000)
+	// samplesReadPerChannel := C.op_read(oggOpusFile,
+	// 	(*C.opus_int16)(&pcm[0]),
+	// 	C.int(cap(pcm)), // / channel_count,
+	// 	&li,
+	// )
+	// if samplesReadPerChannel < 0 {
+	// 	panic(errorFromOpusFileError(samplesReadPerChannel))
+	// }
+
+	// fmt.Println(li, samplesReadPerChannel, samplesReadPerChannel*channel_count)
+	// fmt.Println(pcm[:100])
+	// i := samplesReadPerChannel * channel_count
+	// fmt.Println(pcm[i-20 : i+20])
+
 }
 
 func Test(data []byte) error {
